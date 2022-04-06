ee0459abc2a61ef3600e55788cd3b6a44aab7166
Ross O'Reilly <oreillyross@gmail.com>
2022-04-06 16:59:14 +0200

    add RPN for calculate solution

--- calculate.js
+++ calculate.js
@@ -1,3 +1,6 @@
+// Use Reverse Polish Notation to more easily calculate values
+
+// 5 * 2 / (3 + 4 * 7) - (24 + 2) is 5 2 * 4 7 * 3 + 24 2 + -
 const expression = "5 * 2 / (3 + 4 * 7) - (24 + 2)" // should result in -26
 const simple = "5 - 2 * 2 / 1 - 5"
 
