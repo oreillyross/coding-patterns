f50b16cd26dd2f63a253d5f35e545b1174fda409
Ross O'Reilly <oreillyross@gmail.com>
2022-05-25 19:10:27 +0200

    add commetns

--- linked-list-traversal-algo.py
+++ linked-list-traversal-algo.py
@@ -13,16 +13,21 @@ a.next = b
 b.next = c
 c.next = d
 
+# classic imperative solution
 def printList(head):
 	current = head
 	while current is not None:
 		print(current.val)
 		current = current.next
 
+# recursive classic traversal
 def printList2(head):
+	#base case
 	if head is None:
 		return None
+	# do something with each node
 	print(head.val)
+	#recursion
 	printList2(head.next)
 
 
