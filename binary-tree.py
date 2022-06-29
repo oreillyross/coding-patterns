2f808b2616ba279bbdfb5ec0e3e994ddc701fa87
Faktor 10 <oreillyross@gmail.com>
2022-06-29 13:06:13 +0200

    All tree paths

--- binary-tree.py
+++ binary-tree.py
@@ -16,7 +16,24 @@ def bottom_right_value(root):
       queue.append(node.right)
     if len(queue) == 0:
       return node.val
-      
+
+    
+ # This is an exhaustive dfs solution on a binary tree.   
+def all_tree_paths(root):
+  
+  if root is None:
+    return []
+  if root.left is None and root.right is None:
+    return [[root.val]]
+  
+  paths = []
+  left = all_tree_paths(root.left)
+  for sub in left:
+    paths.append([root.val, *sub])
+  right = all_tree_paths(root.right)
+  for sub in right:
+    paths.append([root.val, *sub])
+  return paths    
       
 a = Node(3)
 b = Node(11)
