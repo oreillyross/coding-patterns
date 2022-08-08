8c6438d9e5c0edda9b7161af98628224f0d73eab
Faktor 10 <oreillyross@gmail.com>
2022-08-08 13:07:27 +0200

    add memoisation

--- sum-paths.py
+++ sum-paths.py
@@ -1,7 +1,11 @@
 def count_paths(grid):
-  return _count_paths(grid, 0, 0)
+  return _count_paths(grid, 0, 0, {})
 
-def _count_paths(grid, r, c):
+def _count_paths(grid, r, c, memo):
+  
+  pos = (r, c)
+  if pos in memo:
+    return memo[pos]
   
   if r == len(grid) or c == len(grid[0]) or grid[r][c] == 'X':
     return 0
@@ -9,9 +13,10 @@ def _count_paths(grid, r, c):
   if r == len(grid) - 1 and c == len(grid[0]) - 1:
     return 1
   
-  bottom = _count_paths(grid, r + 1, c)
-  right = _count_paths(grid, r, c + 1)
-  return bottom + right
+  bottom = _count_paths(grid, r + 1, c, memo)
+  right = _count_paths(grid, r, c + 1, memo)
+  memo[pos] = bottom + right
+  return memo[pos]
   
   
   
