e04f1c63b6d2a3c19ebba2cd5554ed283a6ff224
Replit user <>
2023-06-26 15:13:44 +0000

    add

--- Cookbook/range_function.py
+++ Cookbook/range_function.py
@@ -1,2 +1,10 @@
 # The range function 
 
+captains = ['Piccard', 'Janeway', 'Scott']
+
+for cpt in captains:
+  print(cpt)
+
+# Use it to loop a specific number of times
+for i in range(3):
+  print(i)
