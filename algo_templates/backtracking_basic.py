6c2262aa2b92d8706e71e89d4dcb66aa731de59a
Faktor 10 <oreillyross@gmail.com>
2025-10-07 15:43:24 +0200

    Update backtracking_basic.py

--- algo_templates/backtracking_basic.py
+++ algo_templates/backtracking_basic.py
@@ -1,2 +1,16 @@
-def dfs():
+ans = []
+def dfs(start_index, path, [...additional_states]):
+  if is_leaf(start_index):
+    ans.append(path[:])
+    return
+  for edge in get_edges(start_index, [...additional_states]):
+    if not is_valid(edge):
+      continue
+    path.append(edge)
+    if additional_states:
+      update(...additional_states)
+    dfs(start_index + len(edge), path, [...additional_states])
+    path.pop()
+      
+  
   
