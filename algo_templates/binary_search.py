4a92b5262ed43cf3642b303f69f8d54a03bda93d
Faktor 10 <oreillyross@gmail.com>
2025-10-08 12:08:36 +0200

    Update binary_search.py

--- algo_templates/binary_search.py
+++ algo_templates/binary_search.py
@@ -1,1 +1,11 @@
-
+def binary_search(arr, target):
+  left, right = 0, len(arr) - 1
+  feasible_index = -1
+  while left <= right:
+    mid = (left + right) // 2
+    if feasible(mid):
+      feasible_index = mid
+      right = mid - 1
+    else:
+      left = mid + 1
+  return feasible_index
